40406c7d188a977d73d495a1600d82840348b275
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2023-03-15 13:47:10 +0100

    prepare workflow update to use new artifacts for pro resources

--- scripts/get_latest_circleci_metrics.sh
+++ scripts/get_latest_circleci_metrics.sh
@@ -10,6 +10,18 @@ PROJECT_SLUG="github/localstack/localstack"
 METRICS_RAW="$PARENT_FOLDER/metrics-raw/"
 METRICS_IMPL="$PARENT_FOLDER/metrics-implementation-details/"
 
+# Resulting directory structure required for the create_data_coverage.py
+# - resources/metrics-raw
+#   - community-integration-test.csv (Community) -> this is downloaded from CircleCI (using this script)
+#   - pro-integration-test.csv (Pro)
+#   - moto-integration-test.csv (Moto)
+#   - ...
+# - resources/metrics-implementation-details
+#   - community
+#     - implementation_coverage_full.csv -> this is downloaded from CircleCI (using this script)
+#   - pro
+#     - implementation_coverage_full.csv
+
 echo "Project: $PROJECT_SLUG."
 
 WORKFLOW_ID=$(curl -X 'GET' "https://circleci.com/api/v2/insights/${PROJECT_SLUG}/workflows/main?branch=${METRICS_ARTIFACTS_BRANCH}" -H 'accept: application/json' | jq -r '[.items[] | select(.status == "success")][0].id')
@@ -25,16 +37,6 @@ echo "$ARTIFACT_URLS"
 echo "Downloading metrics data..."
 wget -m --cut-dirs 5 --no-host-directories $ARTIFACT_URLS
 
-# Create the following directory structure for the coverage_docs_utility.py
-# - resources/metrics-raw
-#   - metric-report-raw-data-2022-08-16__11_37_31-d1e519d6.csv (Pro)
-#   - metric-report-raw-data-2022-08-16__11_37_31-d1e51as6.csv (Community)
-# - resources/metrics-implementation-details
-#   - pro
-#     - implementation_coverage_full.csv
-#   - community
-#     - implementation_coverage_full.csv
-
 echo "Moving raw community metrics data to $METRICS_RAW"
 mkdir -p $METRICS_RAW
 mv parity_metrics/metric-report-*.csv $METRICS_RAW/community-integration-test.csv
@@ -43,8 +45,6 @@ echo "Moving community metrics implementation details to $METRICS_IMPL..."
 mkdir -p $METRICS_IMPL
 mv community $METRICS_IMPL
 
-echo "Moving pro metrics implementation details to $METRICS_IMPL..."
-mv pro $METRICS_IMPL
 
 echo "Resulting file structure:"
 tree $PARENT_FOLDER
