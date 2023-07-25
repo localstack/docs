211ca1481c81714098d57f142366b0e35612aa7f
Osama M <49054601+webdev51@users.noreply.github.com>
2023-07-25 13:12:55 +0500

    Added copy button for code snippets

---
+++ static/js/global-script.js
@@ -0,0 +1,19 @@
+function setupCodeBlockCopyButton(){
+  $('.highlight pre').each(function(_i,item){
+    $(item).addClass('code-container');
+    $(item).append('<button class="code-copy-button"><i class="fa fa-copy"></i><span>Copy</span></button>')
+  })
+
+  $('.code-copy-button').on('click',function(e){
+    e.preventDefault();
+    const elem = $(this).closest('.code-container').find('code').clone();
+    elem.find('.command-prefix').remove(); // removing prefix
+    navigator.clipboard.writeText(elem.text());
+    $(this).find('span').text('Copied!');
+    setTimeout(() => {
+      $(this).find('span').text('Copy');
+    }, 2000);
+  })
+}
+
+setupCodeBlockCopyButton();
