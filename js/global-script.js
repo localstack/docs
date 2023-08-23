45c35505c4abf12f47b641f1d4e745d26b2d4292
Nancy-Chauhan <Nancy-Chauhan@users.noreply.github.com>
2023-08-23 09:34:19 +0000

    deploy: 27e1e21390eb5c024d96a2f9b8c081cb6ed208a5

--- js/global-script.js
+++ js/global-script.js
@@ -1,10 +1,10 @@
-function setupCodeBlockCopyButton(){
-  $('.highlight pre').each(function(_i,item){
+function setupCodeBlockCopyButton() {
+  $('.highlight pre').each(function (_i, item) {
     $(item).addClass('code-container');
     $(item).append('<button class="code-copy-button"><i class="fa fa-copy"></i><span>Copy</span></button>')
   })
 
-  $('.code-copy-button').on('click',function(e){
+  $('.code-copy-button').on('click', function (e) {
     e.preventDefault();
     const elem = $(this).closest('.code-container').find('code').clone();
     elem.find('.command-prefix').remove(); // removing prefix
@@ -16,4 +16,14 @@ function setupCodeBlockCopyButton(){
   })
 }
 
+function playYoutubeVideo() {
+  $('.video-thumbnail').on('click', function () {
+    var url = $(this).attr('data-url');
+    var iframeHtml = '<iframe width="100%" src="' + url + '?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    $(this).append(iframeHtml)
+    $(this).addClass('played');
+  })
+}
+
 setupCodeBlockCopyButton();
+playYoutubeVideo();
