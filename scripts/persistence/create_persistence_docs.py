c45a5552bdda5d7e175f371d029b0248f161df92
Giovanni Grano <me@giograno.com>
2024-05-13 18:03:33 -0400

    added function to update the frontmatter

--- scripts/persistence/create_persistence_docs.py
+++ scripts/persistence/create_persistence_docs.py
@@ -1,10 +1,13 @@
 import os
+from io import BytesIO
 import json
 import notion_client as n_client
+import frontmatter
 from notion.catalog import PersistenceCatalog
 
 token = os.getenv("NOTION_TOKEN")
 coverage_path = "../../data/coverage"
+markdown_path = "../../content/en/user-guide/aws"
 
 
 def collect_status() -> dict:
@@ -24,6 +27,8 @@ def collect_status() -> dict:
 
 
 def update_coverage_data():
+    """This function augments the coverage data files (one per service) by adding information
+    about persistence support; such data is obtained from the persistence catalog on Notion"""
     if not token:
         print("Aborting, please provide a NOTION_TOKEN in the env") 
     statuses = collect_status()
@@ -41,5 +46,30 @@ def update_coverage_data():
             json.dump(content, w_file, indent=2)
 
 
+def update_frontmatter():
+    """Updates the frontmatter of the service page in the user guide Markdown file"""
+    coverage_files = [_f for _f in os.listdir(coverage_path) if _f.endswith("json")]
+    if not coverage_files:
+        print("Can't find any coverage file; something went wrong; abort")
+    for cov_file in coverage_files:
+        service = cov_file.split('.')[0]
+        _path = os.path.join(markdown_path, service, "index.md")
+        if not os.path.exists(_path):
+            print(f" Can't find index.md file for {service}")
+            continue
+        
+        # read the status of persistence from the coverage JSON data
+        with open(os.path.join(coverage_path, cov_file), 'r') as f:
+            data = json.loads(f.read())
+        status = data.get("feature_support", {}).get("persistence", {}).get("status", "unknown")
+     
+        # open the markdown file and read the content
+        content = frontmatter.load(_path)
+        content.metadata["persistence"] = status
+        frontmatter.dump(content, _path)
+
+
 if __name__ == "__main__":
     update_coverage_data()
+    update_frontmatter()
+
