afe88b3db27cba30d0cb39dffb78c3161173eee1
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2024-05-23 16:36:43 +0200

    add debug curl

--- scripts/get_latest_circleci_metrics.sh
+++ scripts/get_latest_circleci_metrics.sh
@@ -25,6 +25,7 @@ METRICS_IMPL="$PARENT_FOLDER/metrics-implementation-details/"
 echo "Project: $PROJECT_SLUG."
 
 HEADERS="-H 'accept: application/json' -H 'Circle-Token:$CIRCLE_CI_TOKEN'"
+curl -X 'GET' "https://circleci.com/api/v2/insights/${PROJECT_SLUG}/workflows/main?branch=${METRICS_ARTIFACTS_BRANCH}" "${HEADERS}" 
 
 WORKFLOW_ID=$(curl -X 'GET' "https://circleci.com/api/v2/insights/${PROJECT_SLUG}/workflows/main?branch=${METRICS_ARTIFACTS_BRANCH}" "${HEADERS}" | jq -r '[.items[] | select(.status == "success")][0].id')
 echo "Latest successful workflow: $WORKFLOW_ID"
