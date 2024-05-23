7a3c2701823ac3689449405a1facb56d2969bdb7
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2024-05-23 20:15:17 +0200

    fixup

--- scripts/get_latest_circleci_metrics.sh
+++ scripts/get_latest_circleci_metrics.sh
@@ -24,9 +24,7 @@ METRICS_IMPL="$PARENT_FOLDER/metrics-implementation-details/"
 
 echo "Project: $PROJECT_SLUG."
 
-#  -H 'Circle-Token:${CIRCLE_CI_TOKEN}'
-HEADERS="-H 'accept: application/json'"
-curl -X 'GET' "https://circleci.com/api/v2/insights/${PROJECT_SLUG}/workflows/main?branch=${METRICS_ARTIFACTS_BRANCH}" ${HEADERS} 
+HEADERS="-H 'accept: application/json' -H 'Circle-Token:${CIRCLE_CI_TOKEN}'"
 
 WORKFLOW_ID=$(curl -X 'GET' "https://circleci.com/api/v2/insights/${PROJECT_SLUG}/workflows/main?branch=${METRICS_ARTIFACTS_BRANCH}" "${HEADERS}" | jq -r '[.items[] | select(.status == "success")][0].id')
 echo "Latest successful workflow: $WORKFLOW_ID"
